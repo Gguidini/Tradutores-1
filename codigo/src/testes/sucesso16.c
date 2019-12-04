66b1e22c7c27ba0d493e2846a59c88705d009533
luix <arueluis@gmail.com>
2019-12-04 20:01:24 -0300

    Teste 16 melhor

--- codigo/src/testes/sucesso16.c
+++ codigo/src/testes/sucesso16.c
@@ -1,13 +1,28 @@
 int main(){
 	SumArray<int> s[10];
+	MinArray<int> s1[10];
+	MaxArray<int> s2[10];
 	int i;
 	i = 0;
 	while(i < 10){
+		s1[i, i] += i;
+		s2[i, i] += i;
 		s[i, i] += i;
 		i += 1;
 	}
 	s[1, 3] += 100;
+	s1[1, 3] += 100;
+	s2[1, 3] += 100;
 	int x;
 	x = s[1, 3];
+	outInt x;
+	x = s[1, 1];
+	outInt x;
+	x = s[2, 2];
+	outInt x;
+	x = s[1, 6];
+	outInt x;
+	s[2, 5] += 50;
+	x = s[1, 6];
 	outInt x;
 }
